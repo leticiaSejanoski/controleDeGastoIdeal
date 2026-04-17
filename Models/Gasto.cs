c610ebda03154decaba32251e450204d86db8d66
leticiaSejanoski <131303547+leticiaSejanoski@users.noreply.github.com>
2026-04-17 20:16:03 -0300

    Cria classe Gasto e suas funcionalidades

--- Models/Gasto.cs
+++ Models/Gasto.cs
@@ -3,13 +3,14 @@ class Gasto
 {
     public string descricao;
     public double valor;
-    // private Categoria categoria;
+    public Categoria categoria;
 
     //cria construtor
-    public Gasto(string descricao, double valor)
+    public Gasto(string descricao, double valor, Categoria categoria)
     {
         this.descricao = descricao;
         this.valor = valor;
+        this.categoria = categoria;
     }
 
     //Verifica se os valores inseridos pelo usuário são válidos para adicionar na List
--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void CadastrarCategoria(List<Categoria> categorias)
     {
+        Console.WriteLine("\n===== CADASTRAR CATEGORIA =====");
         Console.Write("Nome: ");
         string nome = Console.ReadLine();
 
@@ -29,7 +30,7 @@ class Program
     static void ListarCategorias(List<Categoria> categorias)
     {
         //verifica se a lista está vazia
-        if (categorias.Count() == 0)
+        if (categorias.Count == 0)
         {
             Console.WriteLine("Lista vazia!");
             return;
@@ -43,31 +44,49 @@ class Program
     }
 
     //Função para cadastrar gasto
-    static void CadastrarGasto(List<Gasto> listaGastos)
+    static void CadastrarGasto(List<Gasto> listaGastos, List<Categoria> categorias)
     {
+        Console.WriteLine("\n===== CADASTRAR GASTO =====");
         Console.Write("Descrição: ");
         string descricao = Console.ReadLine();
 
         Console.Write("Valor: ");
         double valor = double.Parse(Console.ReadLine());
 
-        Gasto gasto = new Gasto(descricao, valor);
+        Console.WriteLine("Selecione uma categoria: ");
+        if (categorias.Count == 0)
+        {
+            Console.WriteLine("Não há nenhuma categoria cadastrada.");
+            return;
+        }
+        int i = 1;
+        foreach (Categoria c in categorias)
+        {
+            Console.WriteLine($"[{i}]" + c.nome);
+            i++;
+        }
+
+        int opcao = int.Parse(Console.ReadLine());
+        Categoria categoriaSelecionada = categorias[opcao-1];
+
+
+        Gasto gasto = new Gasto(descricao, valor, categoriaSelecionada);
 
         if (gasto.ValidaGasto())
         {
             listaGastos.Add(gasto);
+            Console.WriteLine("Gasto adicionado!");
         }
         else
         {
             Console.WriteLine("Erro! Descrição ou valor do gasto inválido.");
         }
-
-        Console.WriteLine("------------------------");
     }
 
     //Função que lista os gastos
     static void ListarGastos(List<Gasto> listaGastos)
     {
+        Console.WriteLine("\n===== GASTOS =====");
         if (listaGastos.Count == 0)
         {
             Console.WriteLine("Nenhum gasto cadastrado.");
@@ -76,7 +95,7 @@ class Program
 
         foreach (Gasto gasto in listaGastos)
         {
-            Console.WriteLine(gasto.descricao + " - R$" + gasto.valor);
+            Console.WriteLine($"{gasto.descricao} - R${gasto.valor} - {gasto.categoria.nome}");
         }
     }
 
@@ -86,6 +105,7 @@ class Program
         List<Categoria> categorias = new List<Categoria>();
 
         List<Gasto> listaGastos = new List<Gasto>();
+
     }
 
 }
