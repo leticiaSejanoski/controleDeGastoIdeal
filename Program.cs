ae86eff2c98c8b4cc0bbdddcffebfb802e031fd6
Maria Isabella <isabelladefranca410@gmail.com>
2026-04-21 14:34:22 -0300

    edita o main e chama funções

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
@@ -93,12 +94,146 @@ class Program
             return;
         }
 
+        int i = 1; //eu add aq pra colocar indice qnd for mostrar os gastos e pra auxiliar na remoção
         foreach (Gasto gasto in listaGastos)
         {
-            Console.WriteLine($"{gasto.descricao} - R${gasto.valor} - {gasto.categoria.nome}");
+            Console.WriteLine($"{i} - {gasto.descricao} - R${gasto.valor} - {gasto.categoria.nome}");
+            i++;
         }
     }
 
+    //funçao que remove categorias e os gastos correspondentes
+    static void RemoverCategoria(List<Categoria> categorias, List<Gasto> listaGastos)
+    {
+        if(categorias.Count == 0)
+        {
+            Console.WriteLine("Nenhuma categoria cadastrada.");
+            return;            
+        }
+
+        Console.WriteLine("===== REMOVER CATEGORIAS =====");        
+        Console.WriteLine("Qual categoria deseja remover?");
+
+        int i = 1;   
+        foreach (Categoria c in categorias)
+        {
+            Console.WriteLine($"{i} -" + c.nome);
+            i++;
+        }
+
+        int opcaoRemover = int.Parse(Console.ReadLine());
+        if (opcaoRemover < 1 || opcaoRemover > categorias.Count)
+        {
+            Console.WriteLine("Opção inválida!");
+            return;
+        }
+        Categoria categoriaRemovida = categorias[opcaoRemover - 1];
+
+        for (int j = listaGastos.Count - 1; j >= 0; j--)
+        {
+            if (listaGastos[j].categoria == categoriaRemovida)
+            {
+                listaGastos.RemoveAt(j);
+            }
+        }
+        categorias.RemoveAt(opcaoRemover - 1);
+        Console.WriteLine("Categoria removida!");       
+    }
+
+    //função que remove gastos
+    static void RemoverGasto(List<Gasto> listaGastos)
+    {
+        if(listaGastos.Count == 0)
+        {
+            Console.WriteLine("Nenhum gasto cadastrado.");
+            return;
+        }
+
+        Console.WriteLine("===== REMOVER GASTOS =====");        
+        Console.WriteLine("Qual gasto deseja remover?");
+
+        ListarGastos(listaGastos);
+
+        int opcaoRemover = int.Parse(Console.ReadLine());
+
+        if (opcaoRemover < 1 || opcaoRemover > listaGastos.Count)
+        {
+            Console.WriteLine("Opção inválida!");
+            return;
+        }
+        listaGastos.RemoveAt(opcaoRemover - 1);
+        Console.WriteLine("Gasto removido!");
+    }
+
+    //função que soma os gastos (sem distinção de categorias)
+    static void TotalGasto(List<Gasto> listaGastos)
+    {
+        if(listaGastos.Count == 0)
+        {
+            Console.WriteLine("Nenhum gasto cadastrado.");
+            return;            
+        }
+        double soma = 0;
+        Console.WriteLine("===== TOTAL DE GASTOS =====");
+        for(int i = 0; i < listaGastos.Count; i++)
+        {
+            soma += listaGastos[i].valor;
+        }
+        Console.WriteLine($"Total de gastos: R$ ${soma:F2}");
+    }
+
+    //função que soma os gastos da mesma categoria
+    static void TotalCategoria(List<Gasto> listaGastos, List<Categoria> categorias)
+    {
+        if (categorias.Count == 0)
+        {
+            Console.WriteLine("Nenhuma categoria cadastrada.");
+            return;
+        }
+        Console.WriteLine("===== TOTAL POR CATEGORIA =====");
+        for (int i = 0; i < categorias.Count; i++)
+        {
+            double soma = 0;
+            for (int j = 0; j < listaGastos.Count; j++)
+            {
+                if (listaGastos[j].categoria == categorias[i])
+                {
+                    soma += listaGastos[j].valor;
+                }
+            }
+            Console.WriteLine(categorias[i].nome + $": R$ ${soma:F2}");
+        }
+    }
+
+
+    //enum para facilitar na construção do menu e recepção dos inputs
+        enum menuOpcao
+    {
+        Cad_Categoria = 1,
+        Cad_Gasto = 2,
+        Listar_Categoria = 3,
+        Listar_Gasto = 4,
+        Total_Categoria = 5,
+        Total_Gasto = 6,
+        Remov_Categoria = 7,
+        Remov_Gasto = 8,
+        Sair = 0
+    }
+
+    //função que mostra o menu
+    static void MostrarMenu()
+    {
+        Console.WriteLine("===== MENU =====");
+        Console.WriteLine("[1] Cadastrar categoria");
+        Console.WriteLine("[2] Cadastrar gasto");
+        Console.WriteLine("[3] Listar categorias");
+        Console.WriteLine("[4] Listar gastos");
+        Console.WriteLine("[5] Total por categoria");
+        Console.WriteLine("[6] Total de gastos");
+        Console.WriteLine("[7] Remover categoria");        
+        Console.WriteLine("[8] Remover gasto");
+        Console.WriteLine("[0] Sair");                                           
+    }
 
     static void Main(string[] args)
     {
@@ -106,6 +241,45 @@ class Program
 
         List<Gasto> listaGastos = new List<Gasto>();
 
+        while (true)
+        {
+            MostrarMenu();
+            int escolha = int.Parse(Console.ReadLine());
+            menuOpcao opcao = (menuOpcao)escolha;
+            switch (opcao)
+            {
+                case menuOpcao.Cad_Categoria:
+                    CadastrarCategoria(categorias);
+                    break;
+                case menuOpcao.Cad_Gasto:
+                    CadastrarGasto(listaGastos,categorias);
+                    break;
+                case menuOpcao.Listar_Categoria:
+                    ListarCategorias(categorias);
+                    break;
+                case menuOpcao.Listar_Gasto:
+                    ListarGastos(listaGastos);
+                    break;
+                case menuOpcao.Total_Categoria:
+                    TotalCategoria(listaGastos, categorias);
+                    break;                    
+                case menuOpcao.Total_Gasto:
+                    TotalGasto(listaGastos);
+                    break;
+                case menuOpcao.Remov_Categoria:
+                    RemoverCategoria(categorias, listaGastos);
+                    break;
+                case menuOpcao.Remov_Gasto:
+                    RemoverGasto(listaGastos);
+                    break;
+                case menuOpcao.Sair:
+                    Console.WriteLine("Sistema encerrado!");
+                    return;
+                default:
+                    Console.WriteLine("Entrada inválida!");
+                    break;
+            }
+        }
     }
 
 }
